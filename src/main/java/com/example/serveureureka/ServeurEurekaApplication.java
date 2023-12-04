cb2866acfe47a4602e8d8e3d0261d06e58c15108
NourMessioui14 <nour.messioui@esprit.tn>
2023-12-04 20:01:42 +0100

    first commit

---
+++ src/main/java/com/example/serveureureka/ServeurEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.example.serveureureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServeurEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurEurekaApplication.class, args);
+	}
+
+}
